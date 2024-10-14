125a3e67ac60a3544a6d147d3003adfa9bcc2084
Amir Abdul Wahid <aa882755@gmail.com>
2024-10-14 23:07:24 +0530

    AdvancedTaxiFarePrediction

--- Beginner_ML/MachineLearningProject/MachineLearningDataBaseFactory/ConfigureService.cs
+++ Beginner_ML/MachineLearningProject/MachineLearningDataBaseFactory/ConfigureService.cs
@@ -10,25 +10,25 @@ public class ConfigureService
     public void ConfigureServices(IServiceCollection services)
     {
         // Add database context for SQL Server, MySQL, PostgreSQL
-        services.AddDbContext<MyDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("SqlServerConnection")));
-        services.AddDbContext<MyDbContext>(options =>
-            options.UseMySql(Configuration.GetConnectionString("MySqlConnection")));
-        services.AddDbContext<MyDbContext>(options =>
-            options.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
+        ////services.AddDbContext<MyDbContext>(options =>
+        ////    options.UseSqlServer(Configuration.GetConnectionString("SqlServerConnection")));
+        ////services.AddDbContext<MyDbContext>(options =>
+        ////    options.UseMySql(Configuration.GetConnectionString("MySqlConnection")));
+        ////services.AddDbContext<MyDbContext>(options =>
+        ////    options.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
 
-        // Register Redis and MongoDB clients
-        services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("localhost"));
-        services.AddSingleton<IMongoClient>(new MongoClient("mongodb://localhost:27017"));
+        ////// Register Redis and MongoDB clients
+        ////services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("localhost"));
+        ////services.AddSingleton<IMongoClient>(new MongoClient("mongodb://localhost:27017"));
 
-        // Register database factories
-        services.AddTransient<SqlServerDatabaseFactory>();
-        services.AddTransient<MySqlDatabaseFactory>();
-        services.AddTransient<MongoDbDatabaseFactory>();
-        services.AddTransient<RedisCacheDatabaseFactory>();
-        //services.AddTransient<PostgreSqlDatabaseFactory>();
+        ////// Register database factories
+        ////services.AddTransient<SqlServerDatabaseFactory>();
+        ////services.AddTransient<MySqlDatabaseFactory>();
+        ////services.AddTransient<MongoDbDatabaseFactory>();
+        ////services.AddTransient<RedisCacheDatabaseFactory>();
+        //////services.AddTransient<PostgreSqlDatabaseFactory>();
 
-        // Register the provider
-        services.AddTransient<DatabaseFactoryProvider>();
+        ////// Register the provider
+        ////services.AddTransient<DatabaseFactoryProvider>();
     }
 }
