44f312ee1691baa148d07e792d5086dbd9c8adc0
Amir Abdul Wahid <aa882755@gmail.com>
2024-10-19 11:51:00 +0530

    DatabaseLoaderConsoleApp

--- MicroSoftExample/CommonLibrary/CommonLib/PathFind.cs
+++ MicroSoftExample/CommonLibrary/CommonLib/PathFind.cs
@@ -31,4 +31,21 @@ public static class PathFind
     ////    string assemblyFolderPath = _dataRoot.Directory!.Parent!.Parent!.Parent!.FullName;
     ////    return $"{assemblyFolderPath}{relativePath}";
     ////}
+    public static string GetProjectRootPath()
+    {
+        // Get the base directory (which includes the bin directory)
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        // Create a DirectoryInfo object to work with the directory structure
+        DirectoryInfo dirInfo = new DirectoryInfo(baseDirectory);
+
+        // Traverse up the directory structure to find the parent of 'bin'
+        while (dirInfo != null && !string.Equals(dirInfo.Name, "bin", StringComparison.OrdinalIgnoreCase))
+        {
+            dirInfo = dirInfo.Parent!;
+        }
+
+        // If 'bin' directory is found, return its parent (the project root)
+        return dirInfo?.Parent?.FullName ?? baseDirectory;
+    }
 }
