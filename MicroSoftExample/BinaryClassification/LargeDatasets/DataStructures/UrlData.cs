31076cd432a41e0500f5662275f50aea26e3d5d8
Amir Abdul Wahid <aa882755@gmail.com>
2024-10-20 18:13:46 +0530

    LargeDatasets

---
+++ MicroSoftExample/BinaryClassification/LargeDatasets/DataStructures/UrlData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace LargeDatasets.DataStructures;
+
+public class UrlData
+{
+    [LoadColumn(0)]
+    public string? LabelColumn;
+    
+    [LoadColumn(1, 3231961)]
+    [VectorType(3231961)]
+    public float[]? FeatureVector;
+}
---
+++ MicroSoftExample/BinaryClassification/LargeDatasets/DataStructures/UrlPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace LargeDatasets.DataStructures;
+
+public class UrlPrediction
+{
+    // ColumnName attribute is used to change the column name from
+    // its default value, which is the name of the field.
+    [ColumnName("PredictedLabel")]
+    public bool Prediction;
+    
+    public float Score;
+}
