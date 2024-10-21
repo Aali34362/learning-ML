b85eab15f5eb9c32a76695fd36693a2a4f4fe76b
Amir Abdul Wahid <aa882755@gmail.com>
2024-10-21 08:36:30 +0530

    Ranking_Web

---
+++ MicroSoftExample/Ranking/Ranking_Web/DataStructures/ConsoleHelper.cs
@@ -0,0 +1,15 @@
+﻿using WebRanking.DataStructures;
+
+namespace Ranking_Web.DataStructures;
+
+public static partial class ConsoleHelper
+{
+    // Prints out the the individual scores used to determine the relative ranking.
+    public static void PrintScores(IEnumerable<SearchResultPrediction> predictions)
+    {
+        foreach (var prediction in predictions)
+        {
+            Console.WriteLine($"GroupId: {prediction.GroupId}, Score: {prediction.Score}");
+        }
+    }
+}
